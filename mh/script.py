f374f1d0c8c7d5bcb4c92d240e087c364c277875
Filippo Guerra <filippoguerra2001@gmail.com>
2024-06-08 10:51:22 +0200

    Finished hw1

--- mh/script.py
+++ mh/script.py
@@ -4,12 +4,17 @@ import random, subprocess
 import argparse
 import signal
 
-def signal_handler(num, frame):
+def sigint(num, frame):
 	print("\nClosing....\n")
 	exit(0)
 
-def main(host):
+def sigstp(num, frame):
+	print("\nResetting network cards")
+	subprocess.call("ifconfig eth0 down && ifconfig eth1 down && ifconfig eth2 down && ifconfig eth3 down", shell=True)
+	print("Closing....\n")
+	exit(0)
 
+def main(host):
 	subprocess.call("ifconfig eth0 down && ifconfig eth1 down && ifconfig eth2 down && ifconfig eth3 down", shell=True)
 
 	while True:
@@ -23,12 +28,16 @@ def main(host):
 		subprocess.call(f"ifconfig eth{interface} down", shell=True)
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(prog="Python mobility simulator", description="This program simulates the movement of an host in python", epilog="------------")
-	parser.add_argument('-ho', '--host', type=str, default="192.168.1.2")
+	parser = argparse.ArgumentParser(prog="Python mobility simulator", description="This program simulates the movement of an host in python", epilog="Use Ctrl+C to close the program and Ctrl+Z to close and reset the network configuration")
+	parser.add_argument('-ho', '--host', type=str, default="192.168.1.1")
 	args = parser.parse_args()
 
 	# SIGINT handler
-	default_handler = signal.getsignal(signal.SIGINT)
-	signal.signal(signal.SIGINT, signal_handler)
+	sigint_handler = signal.getsignal(signal.SIGINT)
+	signal.signal(signal.SIGINT, sigint)
+
+	# SIGSTP handler
+	sigint_handler = signal.getsignal(signal.SIGTSTP)
+	signal.signal(signal.SIGTSTP, sigstp)
 	
 	main(args.host)
