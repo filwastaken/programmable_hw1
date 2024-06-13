2f0379c54d6a38dcc3d3a79ad3ef4efcbe0230ef
Filippo Guerra <filippoguerra2001@gmail.com>
2024-06-13 20:02:13 +0200

    Simplified script to a working point

--- mh/script.py
+++ mh/script.py
@@ -8,20 +8,11 @@ def sigint(num, frame):
 	print("\nClosing....\n")
 	exit(0)
 
-def sigstp(num, frame):
-	print("\nResetting network cards")
-	subprocess.call("ifconfig eth0 down && ifconfig eth1 down && ifconfig eth2 down && ifconfig eth3 down", shell=True)
-	print("Closing....\n")
-	exit(0)
-
 def main(host):
-	subprocess.call("ifconfig eth0 down && ifconfig eth1 down && ifconfig eth2 down && ifconfig eth3 down", shell=True)
-
 	while True:
 		interface = random.randint(0, 3)
 		amount = random.randint(2, 20)
 
-		subprocess.call(f"ifconfig eth{interface} up", shell=True)
 		print(f"Sending {amount} pings to {host} via the interface eth{interface}")
 		subprocess.call(f"ping {host} -c {amount} -I eth{interface}", shell=True)
 		print("----------------------------------------------------------\nDone!\n")
@@ -35,9 +26,5 @@ if __name__ == "__main__":
 	# SIGINT handler
 	sigint_handler = signal.getsignal(signal.SIGINT)
 	signal.signal(signal.SIGINT, sigint)
-
-	# SIGSTP handler
-	sigint_handler = signal.getsignal(signal.SIGTSTP)
-	signal.signal(signal.SIGTSTP, sigstp)
 	
 	main(args.host)
